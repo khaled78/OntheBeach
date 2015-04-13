07b6e3d51dc4edc1c99ef246bff7486ec338890a
khaled78 <khaled.kh78@yahoo.co.uk>
2015-04-13 23:35:45 +0100

    Task1

---
+++ Task1.sql
@@ -0,0 +1,7 @@
+﻿SELECT 
+      e.name Name
+    , CAST(s.Annual_Amount AS VARCHAR(10)) + ' ' +  c.Unit 'Amount with out conversion'
+    , CAST(s.annual_Amount * c.Conversion_Factor as VARCHAR(20)) + ' GBP' 'Amount in GBP'
+FROM Employees e
+JOIN Salaries s ON e.Id = s.Employee_id
+JOIN Currencies c on c.Id = s.Currency
