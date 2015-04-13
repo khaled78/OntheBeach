66f955f57cc0b6005498a57d3823c3e10f67cba2
khaled78 <khaled.kh78@yahoo.co.uk>
2015-04-13 23:45:48 +0100

    sqlquery

---
+++ Task2.sql
@@ -0,0 +1,31 @@
+﻿USE [Onthebeach_Demo]
+GO
+/****** Object:  StoredProcedure [dbo].[AnnualAmountRetrieve_GBP]    Script Date: 13/04/2015 23:40:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Khaled>
+-- Create date: <Create Date,11/04/2015,>
+-- Description:	<Description,OnTheBeach,>
+-- =============================================
+ALTER PROCEDURE [dbo].[AnnualAmountRetrieve_GBP] 
+	-- Add the parameters for the stored procedure here
+	@name nvarchar(255)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT 
+      e.name Name
+    , s.Annual_Amount,c.Unit AS Unit
+    , (s.annual_Amount * c.Conversion_Factor) AS 'Amount In GBP'
+FROM Employees e
+JOIN Salaries s ON e.Id = s.Employee_id
+JOIN Currencies c on c.Id = s.Currency
+Where e.name=@name
+END
---
+++ Task3.sql
@@ -0,0 +1,30 @@
+﻿USE [Onthebeach_Demo]
+GO
+/****** Object:  StoredProcedure [dbo].[StaffLevel]    Script Date: 13/04/2015 23:43:37 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Khaled>
+-- Create date: <Create Date,11/04/2015>
+-- Description:	<Description,OntheBeach>
+-- =============================================
+ALTER PROCEDURE [dbo].[StaffLevel] 
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT
+    e.name AS 'Employee Name',r.Name AS 'Role Name',s.annual_amount AS 'Annual Salary',c.unit AS 'Currencies',(s.annual_amount*c.conversion_factor) AS 'Amount In GBP'
+  FROM Employees e
+  JOIN Roles r on r.id=e.role_id
+  JOIN Salaries s on e.id=s.employee_id
+  JOIN Currencies c on c.id=s.currency
+  ORDER BY s.annual_amount DESC
+END
